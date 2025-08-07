bf2bb8839a608892178c427a5c933bd3b6b81e58
ThomasJager99 <artemzubchenko1@gmail.com>
2025-08-07 19:56:49 +0200

    Change a bit ping for MacOs

--- Cli_projects/Random_scripts/ping_script.sh
+++ Cli_projects/Random_scripts/ping_script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -e 
+#set -e 
 
        	while true; do 
-         PING=$(ping -c 10 google.com)
+        PING=$(ping -c 10 8.8.8.8)
         #TIME=$(echo "$PING" | awk '/time=/{print $8}' | cut -d '=' -f 2)
         #TIME=$(echo "$PING" | awk '/time /{print $10}' | cut -d 'm' -f 1)
         TIME=$(echo "$PING" | awk '/rtt/ {print $4}' | cut -d '/' -f 2)
-        RECEIV=$(echo "$PING" | awk '/received/{print $4}')
-                if (( $(echo "$TIME > 100" | bc -l) )); then
+        RECEIV=$(echo "$PING" | awk '/packets received/{print $4}')
+		#In Linux its just received / in MacOS its packets received
+		if (( $(echo "$TIME > 100" | bc -l) )); then
                 echo "Connection time more then: 100ms" 
                 break
                 elif [ "$RECEIV" -lt 1 ]; then 
